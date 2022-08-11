2dc8dbf82b347a776cfae56ab14dfc3bb5483a43
Elyas RAJJAF <93983897+elyasrajjaf@users.noreply.github.com>
2022-08-11 13:36:27 +0200

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ export default function FourOhFour() {
                 <h1 className='mb-12 px-12 py-4 bg-red-100 border-b border-red-600'>404 - Page non trouvée</h1>
                 <Link href="/">
                     <a className='py-3 px-4 bg-white rounded-xl mt-10 hover:bg-gray-200 transition-colors'>
-                        Retour à l'accueil
+                        Retour
                     </a>
                 </Link>
             </div>
